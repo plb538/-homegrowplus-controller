79999c7284c9e8c4d667aaf71fda80b345296356
Patrick Bonnell <plb538@live.ca>
2018-03-23 16:18:04 +0000

    g

--- main.py
+++ main.py
@@ -73,6 +73,14 @@ def getLightStatuses():
     return jsonify(results), 200
 
 
+@app.route("/info/plants", methods=['GET'])
+def getPlants():
+    cur, con = dbc.connectToDB('localhost', 5432, 'postgres', 'postgres', 'homegrowplus')
+    cur.execute("""SELECT * FROM plants""")
+    results = cur.fetchall()
+    dbc.disconnectFromDB(con)
+    return jsonify(), 200
+
 @app.route("/control/lights", methods=['POST'])
 def setLightStatus():
     if request.json['on'] is True:
@@ -92,23 +100,27 @@ def setPumpStatus():
     results, code = getPumpSensors()
     results = json.loads(results.response[0])
     results = dict([((el[0]), float(el[1])) for el in results])
-    print request.json['pump']
-    t = ""
-    if request.json['pump'].startswith("mi"):
-        t = "a"
-    else:
-        t = "d"
-    if (request.json['pump'] == "clean_water" or request.json['pump'] == "nutrients" or request.json['pump'] == "acid" or request.json['pump'] == "base") and results['mixer_full'] > 0:
-        return jsonify(), 503
-    elif request.json['pump'] == "drain_water":
-        if results['drain_water'] > 0:
+    t = "d"
+    if request.json['on'] == True:
+        if (request.json['pump'] == "clean_water" or request.json['pump'] == "nutrients" or request.json['pump'] == "acid" or request.json['pump'] == "base") and results['mixer_full'] > 2.5:
+            print "Tried to pump with mixer too full"
             return jsonify(), 503
-    elif request.json['pump'] == "mixer":
-        if results['mixer_empty'] == 0:
+        elif request.json['pump'] == "drain_water":
+            if results['drain_water'] > 0:
+                print "Tried to pump with drain too full"
+                return jsonify(), 503
+        elif request.json['pump'] == "mixer":
+            if results['mixer_empty'] < 2.5:
+                print "Tried to pump with mixer empty"
+                return jsonify(), 503
+        elif request.json['pump'] == "mister":
+            if results['clean_water'] == 0:
+                print "Tries to use mister while clean water is low"
+                return jsonify(), 503
+        elif (request.json['pump'] != "drain_water" or request.json['pump'] != "mixer" or request.json['pump'] != "mister") and results[request.json['pump']] == 0:
+            print "Tried to pump with bad low fluid level on {}".format(request.json['pump'])
             return jsonify(), 503
-    elif (request.json['pump'] != "drain_water" or request.json['pump'] != "mixer") and results[request.json['pump']] == 0:
-        return jsonify(), 503
-    if request.json['on'] is True:
+
         sc.turnOn(api, pins["{}_pump".format(request.json['pump'])], t)
     else:
         sc.turnOff(api, pins["{}_pump".format(request.json['pump'])], t)
@@ -128,6 +140,17 @@ def setMixerStatus():
     dbc.disconnectFromDB(con)
     return jsonify(), 200
 
+
+@app.route("/add/plants", methods=['POST'])
+def addPlant():
+    cur, con = dbc.connectToDB('localhost', 5432, 'postgres', 'postgres', 'homegrowplus')
+    cur.execute("""INSERT INTO plants (name, date) values ('{}', {})""".format(request.json['name'], request.json['date']))
+    con.commit()
+    dbc.disconnectFromDB(con)
+    return jsonify(), 200
+#Will send a JSON like below, save it to DB
+#[{"name":"Tomato","datePlanted":1521840695001},{"name":"Cucumber","datePlanted":1521840716209},{"name":"Empty","datePlanted":1521840674489}]
+
 # To do
 @app.route("/management/schedule", methods=['POST'])
 def setSchedule():
@@ -146,71 +169,95 @@ def getSchedule():
 def poll(api):
     #dht = DHT(temp_sensor_0, DHT.DHT11)
     while True:
-        print "polling"
         try:
             cur, con = dbc.connectToDB('localhost', 5432, 'postgres', 'postgres', 'homegrowplus')
-            val = sc.quantize(sc.readPin(api, pH_sensor_0, "a"), 14)
-            cur.execute("""UPDATE ph_sensors SET value = {} WHERE name = '{}'""".format(val, "ph_sensor_0"))
+            #val = sc.quantize(sc.readPin(api, pH_sensor_0, "a"), 14)
+            #cur.execute("""UPDATE ph_sensors SET value = {} WHERE name = '{}'""".format(val, "ph_sensor_0"))
+            #con.commit()
             #cur.execute("""UPDATE temp_sensors SET value = {} WHERE name = '{}'""".format(sc.quantize(sc.readPin(api, temp_sensor_0, "a"), 30), "temp_sensor_0"))
-            val = sc.quantize(sc.readPin(api, clean_water_conductivity_sensor, "d"), 5)
+            val = sc.readPin(api, clean_water_conductivity_sensor, "d")
             cur.execute("""UPDATE conductivity_sensors SET value = {} WHERE name = '{}'""".format(val, "clean_water"))
             if val == 0:
-                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format(false, "clean_water"))
-            val = sc.quantize(sc.readPin(api, drain_water_conductivity_sensor, "d"), 5)
+                print "Safety shutdown for clean_water"
+                sc.turnOff(api, clean_water_pump, "d")
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "clean_water"))
+                con.commit()
+            val = sc.readPin(api, drain_water_conductivity_sensor, "d")
             cur.execute("""UPDATE conductivity_sensors SET value = {} WHERE name = '{}'""".format(val, "drain_water"))
             if val > 0:
-                 cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format(false, "drain_water"))
-            val = sc.quantize(sc.readPin(api, nutrient_conductivity_sensor, "d"), 5)
+                 print "Safety shutdown for drain_water"
+                 sc.turnOff(api, drain_water_pump, "d")
+                 cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "drain_water"))
+                 con.commit()
+            val = sc.readPin(api, nutrient_conductivity_sensor, "d")
             cur.execute("""UPDATE conductivity_sensors SET value = {} WHERE name = '{}'""".format(val, "nutrients"))
             if val == 0:
-                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format(false, "nutrients"))
-            val = sc.quantize(sc.readPin(api, acid_conductivity_sensor, "d"), 5)
+                print "Safety shutdown for nutrients"
+                sc.turnOff(api, nutrients_pump, "d")
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "nutrients"))
+                con.commit()
+            val = sc.quantize(sc.readPin(api, acid_conductivity_sensor, "a"), 5)
             cur.execute("""UPDATE conductivity_sensors SET value = {} WHERE name = '{}'""".format(val, "acid"))
-            if val == 0:
-                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format(false, "acid"))
-            val = sc.quantize(sc.readPin(api, base_conductivity_sensor, "d"), 5)
+            if val < 2.5:
+                print "Safety shutdown for acid"
+                sc.turnOff(api, acid_pump, "d")
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "acid"))
+                con.commit()
+            val = sc.quantize(sc.readPin(api, base_conductivity_sensor, "a"), 5)
             cur.execute("""UPDATE conductivity_sensors SET value = {} WHERE name = '{}'""".format(val, "base"))
-            if val == 0:
-                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format(false, "base"))
-            val = sc.quantize(sc.readPin(api, mixer_empty_conductivity_sensor, "d"), 5)
+            if val < 2.5:
+                print "Safety shutdown for base"
+                sc.turnOff(api, base_pump, "d")
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "base"))
+                con.commit()
+            val = sc.quantize(sc.readPin(api, mixer_empty_conductivity_sensor, "a"), 5)
             cur.execute("""UPDATE conductivity_sensors SET value = {} WHERE name = '{}'""".format(val, "mixer_empty"))
-            if val == 0:
-                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format(false, "mixer"))
-            val = sc.quantize(sc.readPin(api, mixer_full_conductivity_sensor, "d"), 5)
+            if val < 2.5:
+                print "Safety shutdown for mixer"
+                sc.turnOff(api, mixer_pump, "d")
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "mixer"))
+                con.commit()
+            val = sc.quantize(sc.readPin(api, mixer_full_conductivity_sensor, "a"), 5)
             cur.execute("""UPDATE conductivity_sensors SET value = {} WHERE name = '{}'""".format(val, "mixer_full"))
+            if val > 2.5:
+                print "Safety shutdown due to mixer overflow"
+                sc.turnOff(api, clean_water_pump, "d")
+                sc.turnOff(api, nutrients_pump, "d")
+                sc.turnOff(api, acid_pump, "d")
+                sc.turnOff(api, base_pump, "d")
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "clean_water"))
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "nutrients"))
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "acid"))
+                cur.execute("""UPDATE pumps SET status = {} WHERE name = '{}'""".format('false', "base"))
+                con.commit()
             con.commit()
         except Exception as e:
             print "Could not poll data from the Arduino"
+            print e
         finally:
             dbc.disconnectFromDB(con)
-            time.sleep(1)
+            time.sleep(2)
 
 
 if __name__ == "__main__":
     print "Starting"
     try:
         api = sc.connectToArduino()
-	# Initialize analog pins
-	# Analog read = 0-1023
-	# Analog write = 0-255
 	pH_sensor_0 = 0
 	temp_sensor_0 = 1
-	light_0 = 2
-	mixer_0 = 3
-	mixer_pump = 4 #Set duty to 255
-	mister_pump = 5 #Set duty to 255
+	acid_conductivity_sensor = 2
+        base_conductivity_sensor = 3
+        mixer_empty_conductivity_sensor = 4
+        mixer_full_conductivity_sensor = 5
 
-        # Initialize digital pins
-	# HIGH >= 2.5V
-	# Conductivity sensors either high or low (use digital pins)
 	clean_water_conductivity_sensor = 2
 	drain_water_conductivity_sensor = 3
 	nutrient_conductivity_sensor = 4
-	acid_conductivity_sensor = 5
-        base_conductivity_sensor = 6
-	mixer_empty_conductivity_sensor = 7
-	mixer_full_conductivity_sensor = 8
-	clean_water_pump = 9
+	light_0 = 5
+        mixer_0 = 6
+        mixer_pump = 7
+        mister_pump = 8
+        clean_water_pump = 9
 	drain_water_pump = 10
 	nutrients_pump = 11
 	acid_pump = 12
@@ -221,7 +268,6 @@ if __name__ == "__main__":
 	# Set pin modes
 	api.pinMode(pH_sensor_0, api.INPUT)
 	api.pinMode(temp_sensor_0, api.INPUT)
-	api.pinMode(light_0, api.OUTPUT)
         api.pinMode(mixer_0, api.OUTPUT)
         api.pinMode(clean_water_conductivity_sensor, api.INPUT)
         api.pinMode(drain_water_conductivity_sensor, api.INPUT)
@@ -242,6 +288,7 @@ if __name__ == "__main__":
 	p.start()
 	app.run(host='0.0.0.0', port=5000, threaded=True)
     except Exception as e:
+
         p.join()
     finally:
         for pin in pins:
